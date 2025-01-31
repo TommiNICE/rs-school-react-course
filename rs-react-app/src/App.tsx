0944fbf12d10b022b16187c392b23301ffe905dd
tomi <tomislav.varga@icloud.com>
2025-01-31 08:41:59 +0100

    refactor: removed comments

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import './App.css';
 import SearchSection from './components/sections/SearchSection';
 import ResultSection from './components/sections/ResultSection';
@@ -22,14 +22,10 @@ class App extends Component<object, AppState> {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* You can add a header component here if needed */}
-        </header>
         <main>
           <SearchSection onSearchResults={this.handleSearchResults} />
           <ResultSection results={this.state.searchResults} />
         </main>
-        <footer>{/* You can add a footer component here if needed */}</footer>
       </div>
     );
   }
