99b85e4a1c2ebb95f92cde48e3f185ee5dd42d2a
tomi <tomislav.varga@icloud.com>
2025-02-02 07:42:58 +0100

    add: files for deployment on fly.io

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rs-school-react-course/rs-react-app/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-      },
-    },
-  },
 });
