14c42b3c69482a09ac377fe89baaea966d5d3db8
tomi <tomislav.varga@icloud.com>
2025-02-02 06:15:55 +0100

    refactor: settings for github pages

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rs-school-react-course/rs-react-app',
+  base: '/rs-school-react-course/rs-react-app/',
 });
