4265329904dd6e4306d8ce2cb75253e6eec32746
tomi <tomislav.varga@icloud.com>
2025-02-02 06:24:53 +0100

    refactor: settings github pages

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/rs-school-react-course/rs-react-app/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
 });
